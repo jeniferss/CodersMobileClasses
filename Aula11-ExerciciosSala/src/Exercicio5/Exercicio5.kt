c2ea33dceb99eeed46515c0f634c683c0331b537
Jenifer Soares <jey.souza11@gmail.com>
2020-09-22 01:18:19 -0300

    Adicionar Exercicios Aula 11

---
+++ Aula11-ExerciciosSala/src/Exercicio5/Exercicio5.kt
@@ -0,0 +1,16 @@
+package Exercicio5
+
+fun main(){
+    val arrayString = ArrayList<String>()
+
+    arrayString.add("Pato")
+    arrayString.add("Cachorro")
+    arrayString.add("Gato")
+
+
+    try {
+        println(arrayString[3])
+    } catch (ex: IndexOutOfBoundsException){
+        ex.printStackTrace()
+    }
+}
---
+++ Aula11-ExerciciosSala/src/Exercicio6/Exercicio6.kt
@@ -0,0 +1,17 @@
+package Exercicio6
+
+fun main(){
+
+    val arrayString: ArrayList<String>? = null
+
+    arrayString?.add("Pato")
+    arrayString?.add("Cachorro")
+    arrayString?.add("Gato")
+
+    try {
+        println(arrayString?.get(3))
+    } catch (ex: Exception){
+        ex.printStackTrace()
+    }
+
+}
